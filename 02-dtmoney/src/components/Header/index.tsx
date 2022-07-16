edd9597e787d90ac32af7e82988926de2e003ed2
Fabio Freitas <fabiofreitasdrey@gmail.com>
2022-07-15 22:07:34 -0300

    chore: add axios and miragejs

--- 02-dtmoney/src/components/Header/index.tsx
+++ 02-dtmoney/src/components/Header/index.tsx
@@ -1,6 +1,18 @@
+import { useEffect, useState } from 'react'
 import logoImg from "../../assets/Logo.svg"
+import ReactModal from 'react-modal'
 import { Container, Content } from './style'
 export function Header(){
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false)
+
+  function handleOpenNewTransactionModalOpen(){
+    setIsNewTransactionModalOpen(true)
+  }
+
+  function handleCloseNewTransactionModalOpen(){
+    setIsNewTransactionModalOpen(false)
+  }
+
   return (
     <Container>
       <Content>
--- 02-dtmoney/src/index.tsx
+++ 02-dtmoney/src/index.tsx
@@ -1,6 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './App';
+import { createServer } from 'miragejs';
+
+createServer({
+  routes(){
+    this.namespace = 'api';
+    this.get('/transactions', ()=>{
+      return [{
+        id: 1,
+        tittle: 'Transaction 1',
+        amount: 400,
+        type: 'deposit',
+        category: 'Food',
+        createdAt: '12/06/2022'
+      }
+      ]
+    })
+  }
+})
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
